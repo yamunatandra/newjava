792f4aa3869cb73d04a7b53c390e715685318b97
yamuna <yamuna.tandra12@gmail.com>
2019-12-06 12:06:48 +0530

    new java file

--- newjava.java
+++ newjava.java
@@ -1,14 +1,24 @@
-import java.util.Scanner;
-public class Multiplication_Table 
+   public class newjava
+
 {
-    public static void main(String[] args) 
-    {
-        Scanner s = new Scanner(System.in);
-	System.out.print("Enter number:");        
-	int n=s.nextInt();
-        for(int i=1; i <= 10; i++)
-        {
-            System.out.println(n+" * "+i+" = "+n*i);
-        }
-    }
+
+public static void main (String args[])
+
+{
+
+System.out.println("Java Hello World");
+
 }
+
+} 
+
+
+
+
+
+
+
+
+
+
+
